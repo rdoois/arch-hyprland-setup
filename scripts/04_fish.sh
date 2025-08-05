99c4038663ca969afc7459184c78f9fd52975c5f
Renato Oliveira <renato.dev.oliveira@gmail.com>
2025-08-05 18:00:20 -0300

    feat: add `fish` script

---
+++ scripts/04_fish.sh
@@ -0,0 +1,8 @@
+#!/bin/fish
+
+# Copy source and alias content to config.fish
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+fisher install jorgebucaran/fisher
+fisher install PatrickF1/fzf.fish
+fisher install jethrokuan/z
