aa120a6d28547c1d81015f60c912cb797ef564d8
Renato Oliveira <renato.dev.oliveira@gmail.com>
2025-08-05 18:00:07 -0300

    feat: add `asdf` script

---
+++ scripts/03_asdf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -A plugins=(
+	["awscli"]="latest"
+	["golang"]="latest"
+	["helm"]="latest"
+	["java"]="openjdk-24.0.2"
+	["k6"]="latest"
+	["kops"]="latest"
+	["kubectl"]="latest"
+	["maven"]="latest"
+	["minikube"]="latest"
+	["nodejs"]="latest"
+	["python"]="latest"
+	["terraform"]="latest"
+)
+
+for plugin in "${!plugins[@]}"; do
+	version=${plugins[$plugin]} 	
+ 	asdf plugin add $plugin
+ 	asdf install $plugin $version
+ 	asdf set -u $plugin $version
+done
+
+asdf completion fish > ~/.config/fish/completions/asdf.fish
