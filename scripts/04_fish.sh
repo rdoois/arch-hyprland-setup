68210862df9ed3519d6f6270082a1127524fffdb
Renato Oliveira <renato.dev.oliveira@gmail.com>
2025-08-05 19:17:04 -0300

    refactor: remove comments from `fish` script

--- scripts/04_fish.sh
+++ scripts/04_fish.sh
@@ -1,7 +1,5 @@
 #!/bin/fish
 
-# Copy source and alias content to config.fish
-
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
 fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
